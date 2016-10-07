172f51c2f59dc6263e734e9bf619f111277ce3dd
AlphaKretin <deewhydeeecks@gmail.com>
2016-10-07 21:52:15 +1030

    improved debug
    console logs instead of alerts

--- js/mirrors.js
+++ js/mirrors.js
@@ -1,21 +1,21 @@
 var B2state = 0;
 var B2image = "blank";
 document.getElementById("B2").addEventListener("click", function() { //to be called onclick
-	alert(B2state); //testing when called
+	console.log(B2state); //debug
 	++B2state;
-	alert(B2state);
+	console.log(B2state); //debug
 	if (B2state > 2 || B2state < 0) { //keeps state looping around from 0-2
 		B2state = 0;
 	}
-	alert(B2state);
+	console.log(B2state); //debug
 	switch (B2state) {
 		case 0: B2image = "blank"; break;
 		case 1: B2image = "slash"; break;
 		case 2: B2image = "backslash"; break;
 		default: B2image = "blank"; alert("state out of bounds"); break; //should never happen, hence why i have it tell me if so
 	}
-	alert(B2image);
+	console.log(B2image); //debug
 	document.getElementById("B2").InnerHTML = "<img src=\"../images/mirrors/" + B2image + ".png\" />";
-	alert(document.getElementById("B2").InnerHTML);
+	console.log(document.getElementById("B2").InnerHTML); //debug
 }, false);
 
