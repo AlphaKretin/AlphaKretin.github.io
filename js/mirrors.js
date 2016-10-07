a0b002a4260cc0764d20852bf9d5c680d4329e69
AlphaKretin <deewhydeeecks@gmail.com>
2016-10-07 18:01:04 +1030

    started scripting
    moved images, started trying to get the script for one cell working, no
    luck so far

--- js/mirrors.js
+++ js/mirrors.js
@@ -1,12 +1,20 @@
-function update(sender) { //to be called onclick at each cell where sender is the cell's ID
-	state++; //TODO make this the state of a given cell, preferable without making 25 vars and functions
-	if (state > 2 || state < 0) { //keeps state looping around from 0-2
-		state = 0;
+var B2state = 0;
+var B2image = "blank";
+document.getElementById("B2").addEventListener("click", updateB2(), false);
+
+function updateB2() { //to be called onclick
+	alert("butts"); //testing when called
+	B2state++;
+	if (B2state > 2 || B2state < 0) { //keeps state looping around from 0-2
+		B2state = 0;
 	}
-	switch (state) {
-		case 0: var image = "blank"; break;
-		case 1: var image = "slash"; break;
-		case 2: var image = "backslash"; break;
-		default: var image = "blank"; alert("state out of bounds"); break; //should never happen, hence why i have it tell me if so
+	switch (B2state) {
+		case 0: B2image = "blank"; break;
+		case 1: B2image = "slash"; break;
+		case 2: B2image = "backslash"; break;
+		default: B2image = "blank"; alert("state out of bounds"); break; //should never happen, hence why i have it tell me if so
 	}
-	document.getElementByID(sender).innerHTML = "<img src=\"../images/mirrors/" + image + ".png\" />"; 
+	document.getElementById("B2").InnerHTML = "<img src=\"../images/mirrors/" + B2image + ".png\" />"; 
+	document.getElementById("links").InnerHTML = B2state;
+}
+	
