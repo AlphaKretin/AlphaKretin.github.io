7d5df5d8ecbd7b51d92bc9c4dde6fe19a486b81c
AlphaKretin <deewhydeeecks@gmail.com>
2016-11-15 13:41:02 +1030

    Update pokemon-function-test.js

--- pokemon-function-test.js
+++ pokemon-function-test.js
@@ -9,7 +9,7 @@ function clicked(){
 function c(str)
 {
 	var s = str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-	s = s.replace(/[[:blank:]]/g,"_");
+	s = s.replace(/ /g,"_");
     return s;	
 }
 
