01ebfc6e7d9edb28be1d528155439d65c5c01b10
AlphaKretin <deewhydeeecks@gmail.com>
2017-02-19 20:58:35 +1030

    image handling delay fix fix

--- js/fetext.js
+++ js/fetext.js
@@ -1,4 +1,4 @@
-console.log("revision 5");
+console.log("revision 6");
 var legalChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "~", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "!", "(", ")", "-", "/", "\\", "?", ";", ":", "#", "&", "\"", "'", "+", "%", "\n"];
 var ctx = document.getElementById("renderCanvas").getContext("2d");
 var input = document.getElementById("inputArea");
@@ -173,7 +173,7 @@ for (var i = 0; i < images.length; i++){
 	images[i].addEventListener("onready", function() {
 		images[i].src = "../images/fetext/" + charToName(legalChars[i]) + ".png";
 		console.log("finished loading" + legalChars[i]);
-	}
+	});
 }
 
 function charToName(letter) {
