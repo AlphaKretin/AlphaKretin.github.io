e2224bb7a027849e19345a4cc091832bb06d5b06
AlphaKretin <deewhydeeecks@gmail.com>
2017-02-19 15:15:53 +1030

    logging

--- js/fetext.js
+++ js/fetext.js
@@ -1,3 +1,4 @@
+console.log("started loading");
 var legalChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "~", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "!", "(", ")", "-", "/", "\\", "?", ";", ":", "#", "&", "\"", "'", "+", "%", "\n"];
 var ctx = document.getElementById("renderCanvas").getContext("2d");
 var input = document.getElementById("inputArea");
@@ -132,3 +133,5 @@ function render() {
 		}
 	}
 }
+
+console.log("finished loading");
