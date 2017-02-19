4e31b31b65048e20346b6273c93918184cbd4021
AlphaKretin <deewhydeeecks@gmail.com>
2017-02-19 23:05:24 +1030

    a

--- arraymaker.js
+++ arraymaker.js
@@ -81,15 +81,15 @@ var charWidths = {
     plus: 8,
     percent: 8
 };
-console.log("chars3");
+console.log("chars4");
 var legalChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "~", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "!", "(", ")", "-", "/", "\\", "?", ";", ":", "#", "&", "\"", "'", "+", "%", "\n"];
 
 function clicked(){
 	var index = 0;
 	var out = "var charWidths = {<br />";
-	var i = 0;
+	var ind = 0;
 	for (var cha of charWidths){
-		out += charToName(legalChars[i]) + ": { wid: " + cha + ", index: " + index + " },<br />";
+		out += charToName(legalChars[ind]) + ": { wid: " + cha + ", index: " + index + " },<br />";
 		index += cha;
 		i++;
 	}
