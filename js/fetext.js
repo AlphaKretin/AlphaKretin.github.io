ac0dd5a1167d96300e158c97da3d0238a908763c
AlphaKretin <deewhydeeecks@gmail.com>
2017-02-19 20:51:37 +1030

    image handling delay

--- js/fetext.js
+++ js/fetext.js
@@ -1,4 +1,4 @@
-console.log("revision 3");
+console.log("revision 4");
 var legalChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "~", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "!", "(", ")", "-", "/", "\\", "?", ";", ":", "#", "&", "\"", "'", "+", "%", "\n"];
 var ctx = document.getElementById("renderCanvas").getContext("2d");
 var input = document.getElementById("inputArea");
@@ -169,9 +169,12 @@ var images = {
     percent: new Image(charWidths["percent"], 15)
 };
 
-for (var i = 0; i < images.length; i++){
-	images[i].src = "../images/fetext/" + charToName(legalChars[i]) + ".png";
-}
+images.addEventListener("onready", function() {
+	for (var i = 0; i < images.length; i++){
+		images[i].src = "../images/fetext/" + charToName(legalChars[i]) + ".png";
+	}
+	console.log("finished loading");
+});
 
 function charToName(letter) {
     switch (letter) {
@@ -293,5 +296,3 @@ function render() {
         }
     }
 }
-
-console.log("finished loading");
