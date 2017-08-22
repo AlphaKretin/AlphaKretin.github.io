63b9b7d3489b44f070ffad5be5b4fbac0384fc3e
AlphaKretin <deewhydeeecks@gmail.com>
2017-08-22 09:54:10 +0930

    Add hidden element support

--- textformat.js
+++ textformat.js
@@ -1,17 +1,20 @@
 function format(text){
 	var lines = text.split("\n");
+	var name = lines[lines.length - 1];
 	lines[0] = "<p><h2>" + lines[0] + "</h2>";
-	lines[1] = "<h3>" + lines[1] + "</h3><ol>";
+	lines[1] = "<h3>" + lines[1] + "</h3><input type=\"button\" value=\"Show\" onclick=\"unhide(" + name + ")\" id=\"" + name + "\" /><ol>";
 	var i;
-	for (i = 2; i < lines.length - 1; i++) {
+	for (i = 2; i < lines.length - 2; i++) {
 		lines[i] = "<li>" + lines[i].slice(5) + "</li>";
 	}
-	lines[lines.length - 1] = "</ol><b>" + lines[lines.length - 1].slice(2) + "</b></p>";
+	lines[lines.length - 2] = "</ol><b>" + lines[lines.length - 2].slice(2) + "</b></p>";
 	var out = "";
 	for (var line of lines){
-		out += line;
+		if (line !== lines[lines.length - 1]){
+			out += line;
+		}
 	}
 	console.log(out);
 }
 
-console.log("loaded");
+console.log("loaded 10am");
