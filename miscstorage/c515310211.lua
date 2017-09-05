85f5c2e2ba03f079d406a2fd5845536dd9892f7c
AlphaKretin <deewhydeeecks@gmail.com>
2017-09-05 12:53:28 +0930

    Create c515310211.lua

---
+++ miscstorage/c515310211.lua
@@ -0,0 +1,26 @@
+--Tinsight Salvo
+--AlphaKretin
+function c515310211.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetTarget(c515310211.target)
+	e1:SetOperation(c515310211.operation)
+	c:RegisterEffect(c1)
+end
+function c515310211.filter(c)
+	return c:IsSetCard(0xf35) and c:IsFaceup()
+end
+function c515310211.target()
+	if chkc then return chkc:IsLocation(LOCATION_SZONE) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_SZONE,LOCATION_SZONE,1,e:GetHandler()) 
+		and Duel.IsExistingMatchingCard(c515310211.filter,tp,LOCATION_ONFIELD,0,1,nil) end
+	local num=Duel.GetMatchingGroupCount(c515310211.filter,tp,LOCATION_ONFIELD,0,e:GetHandler())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_SZONE,LOCATION_SZONE,1,num,e:GetHandler()
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+end
+function c515310211.operation()
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	Duel.Destroy(tg,REASON_EFFECT)
+end
