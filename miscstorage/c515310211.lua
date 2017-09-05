9cf384578068f0fc12b7897f847c7fc941990a43
AlphaKretin <deewhydeeecks@gmail.com>
2017-09-05 13:16:57 +0930

    Update c515310211.lua

--- miscstorage/c515310211.lua
+++ miscstorage/c515310211.lua
@@ -4,6 +4,8 @@ function c515310211.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(c515310211.target)
 	e1:SetOperation(c515310211.operation)
 	c:RegisterEffect(c1)
@@ -11,7 +13,7 @@ end
 function c515310211.filter(c)
 	return c:IsSetCard(0xf35) and c:IsFaceup()
 end
-function c515310211.target()
+function c515310211.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_SZONE) end
 	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_SZONE,LOCATION_SZONE,1,e:GetHandler()) 
 		and Duel.IsExistingMatchingCard(c515310211.filter,tp,LOCATION_ONFIELD,0,1,nil) end
@@ -20,7 +22,7 @@ function c515310211.target()
 	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_SZONE,LOCATION_SZONE,1,num,e:GetHandler()
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 end
-function c515310211.operation()
+function c515310211.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
 	Duel.Destroy(tg,REASON_EFFECT)
 end
