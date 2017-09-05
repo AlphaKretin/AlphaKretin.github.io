9636e282284295d962132c523b497c2d0b1c6007
AlphaKretin <deewhydeeecks@gmail.com>
2017-09-05 12:53:09 +0930

    Update c515310210.lua

--- miscstorage/c515310210.lua
+++ miscstorage/c515310210.lua
@@ -2,6 +2,7 @@
 --AlphaKretin
 function c515310210.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_TODECK+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCost(c515310210.cost)
 	e1:SetTarget(c515310210.target)
