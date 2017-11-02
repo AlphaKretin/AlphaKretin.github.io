16f6a6f3a1deeed5785b8ac27155eb6077d33802
AlphaKretin <deewhydeeecks@gmail.com>
2017-11-02 16:42:21 +1030

    Add random card script

---
+++ js/randcard.js
@@ -0,0 +1,33 @@
+var sql = window.SQL;
+var loaded = false;
+//open database
+var xhr = new XMLHttpRequest();
+xhr.open('GET', '../images/cards.cdb', true);
+xhr.responseType = 'arraybuffer';
+
+var contents;
+var names;
+var ids = [];
+
+xhr.onload = function(e) {
+  var uInt8Array = new Uint8Array(this.response);
+  var db = new SQL.Database(uInt8Array);
+  contents = db.exec("SELECT * FROM datas");
+  names = db.exec("SELECT * FROM texts");
+  for (var card of contents[0]["values"]) {
+	  ids.push(card[0]);
+  }
+  loaded = true;
+  // contents is now [{columns:['col1','col2',...], values:[[first row], [second row], ...]}]
+};
+xhr.send();
+
+function convert() {
+	if (!loaded) {
+		alert("Card database still loading, please wait a few seconds and try again.");
+		return;
+	}
+	var rand = ids[Math.floor(Math.random() * ids.length)];
+	var out = "<h1>" + names[0]["values"][rand][1] + "</h1>" + rand;
+	document.getElementById("output").innerHTML = out;
+}
